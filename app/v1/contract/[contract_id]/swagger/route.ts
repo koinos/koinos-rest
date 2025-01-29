0dca9db66755f45c6756b10d3a91b7da7f0dcc50
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2025-01-29 12:48:44 -0700

    #28: Add proper swagger documentation for the swagger path

--- app/v1/contract/[contract_id]/swagger/route.ts
+++ app/v1/contract/[contract_id]/swagger/route.ts
@@ -3,7 +3,7 @@ import { getContractSwagger } from '@/utils/swagger'
 
 /**
  * @swagger
- * /v1/contract/{contract_id}/abi:
+ * /v1/contract/{contract_id}/swagger:
  *   get:
  *     tags: [Contracts]
  *     description: Returns a Swagger file for the Contract's methods
@@ -16,6 +16,22 @@ import { getContractSwagger } from '@/utils/swagger'
  *        description: Koinos address of the contract, name of the contract (for system contracts) or KAP name
  *        required: true
  *        example: 15DJN4a8SgrbGhhGksSBASiSYjGnMU8dGL
+ *     responses:
+ *       200:
+ *         description: Swagger
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *             example:
+ *               openapi: 3.0.0
+ *               info:
+ *                 title: "'15DJN4a8SgrbGhhGksSBASiSYjGnMU8dGL' REST API"
+ *                 version: 1.0.0
+ *               tags:
+ *               - name: Contracts
+ *                 description: Includes endpoints for interacting with the '15DJN4a8SgrbGhhGksSBASiSYjGnMU8dGL' contract on Koinos
+ *               paths: {}
  */
 
 export async function GET(request: Request, { params }: { params: { contract_id: string } }) {
